bcb798c1b5e9144a396fd51ca49e321fc06e4cee
snesjhon <snesjhon@gmail.com>
2022-08-03 17:22:14 -0700

    feat: another spin test

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env zsh
 
-install_dependencies(){
-echo "\n -- installing plug --"
+if [$SPIN]; then
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-}
-
-if ["$SPIN"]; then
-  install_dependencies
 fi
