a6d4704b01d4d447f6e4225a9119841fa39d262d
snesjhon <snesjhon@gmail.com>
2022-08-03 18:15:32 -0700

    feat: add better handling of plug install

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ if [ $SPIN ]; then
   curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+  nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
+
   ln -sf /home/spin/dotfiles/nvim /home/spin/.config/nvim
 fi
