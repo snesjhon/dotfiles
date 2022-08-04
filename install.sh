75f81b08fe2b01dfeb1e071580a9b4f8216b5d26
snesjhon <snesjhon@gmail.com>
2022-08-03 17:29:17 -0700

    feat: try again

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if [$SPIN]; then
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+if [ $SPIN ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
