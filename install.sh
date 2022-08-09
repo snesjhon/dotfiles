0fa81e2b3131e5487ba5559eaddedd344fbb4eec
snesjhon <snesjhon@gmail.com>
2022-08-09 11:51:15 -0700

    feat: update install.sh as well

--- install.sh
+++ install.sh
@@ -12,18 +12,13 @@ install_packer(){
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim &> /dev/null
 }
 
-install_dependencies(){
-  nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
-}
-
 if [ $SPIN ]; then
-  install_packer
   install_language_servers
 
-  nvim --headless \
-   -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &> /dev/null
-
+  # link configs into vim
   ln -sf /home/spin/dotfiles/nvim /home/spin/.config/nvim
+
+  install_packer
 fi
 
 
