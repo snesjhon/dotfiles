005986072b35fdb75d34f55eb31713e24aa21f53
snesjhon <snesjhon@gmail.com>
2023-02-11 15:57:30 -0800

    feat: add git ignore and other bindings

--- nvim/.config/nvim/after/plugin/defaults.lua
+++ nvim/.config/nvim/after/plugin/defaults.lua
@@ -1,3 +1,12 @@
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+-- Tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.keymap.set('n', '<leader>ef', require("nvim-tree").focus, { desc = "[E]xplorer [F]ocus" })
+vim.keymap.set('n', '<leader>ee', require("nvim-tree").toggle, { desc = "[E]xplorer Toggle" })
 
-vim.cmd [[colorscheme github_dark]]
+-- Terminal Colors
+vim.opt.termguicolors = true
+vim.cmd [[colorscheme github_light]]
+
+-- keymaps
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,23 +1,36 @@
 return function(use)
-  use({
-      "projekt0n/github-nvim-theme",
-      config = function()
-        require("github-theme").setup({
-            theme_style = "dark",
-            function_style = "italic",
-            sidebars = { "qf", "vista_kind", "terminal", "packer" },
-            -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-            colors = { hint = "orange", error = "#ff0000" },
-            -- Overwrite the highlight groups
-            overrides = function(c)
-              return {
-                  htmlTag = { fg = c.red, bg = "#282c34", sp = c.hint, style = "underline" },
-                  DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-                  -- this will remove the highlight groups
-                  TSField = {},
-              }
-            end
-        })
-      end
-  })
+    use({
+        "projekt0n/github-nvim-theme",
+        config = function()
+            require("github-theme").setup({
+                theme_style = "light",
+            })
+        end
+    })
+    use({
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require("nvim-tree").setup({
+                sort_by = "case_sensitive",
+                view = {
+                    side = "right",
+                    width = 30,
+                    mappings = {
+                        list = {
+                            { key = "u", action = "dir_up" },
+                        },
+                    },
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = true,
+                },
+            })
+        end
+    })
 end
