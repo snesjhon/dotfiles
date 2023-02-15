8ced26706f3712197758a5dce1a08320bd581d58
snesjhon <snesjhon@gmail.com>
2023-02-15 15:15:27 -0800

    feat: add further modifications and slight modifications to init

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -40,11 +40,11 @@ return function(use)
             auto_dark_mode.setup({
                 update_interval = 1000,
                 set_dark_mode = function()
-                    vim.api.nvim_set_option('background', 'dark')
+                    vim.api.nvim_set_option('background', 'github_dark')
                     vim.cmd('colorscheme github_dark')
                 end,
                 set_light_mode = function()
-                    vim.api.nvim_set_option('background', 'light')
+                    vim.api.nvim_set_option('background', 'github_light')
                     vim.cmd('colorscheme github_dark')
                 end,
             })
