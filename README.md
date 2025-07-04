5e09ba6d107712e2ff0db6d748765383ddd7f1c5
snesjhon <snesjhon@gmail.com>
2025-07-04 02:21:39 -0700

    adding wezterm

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - nvim
 - starship
+- wezterm
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,36 +1,36 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local colors = require("colors")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
 config.font_size = 15
--- config.color_scheme = "Github Dark"
+
+config.color_schemes = colors
 
 local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return "Dark"
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "GitHub Dark"
-  else
-    return "Github (base16)"
-  end
+	if appearance:find("Dark") then
+		return "Github Dark Default"
+	else
+		return "Github Light Default"
+	end
 end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
-config.window_decorations = "RESIZE"
 
--- Finally, return the configuration to wezterm:
+config.window_decorations = "RESIZE"
+config.font = wezterm.font("FiraCode Nerd Font")
+
 return config
